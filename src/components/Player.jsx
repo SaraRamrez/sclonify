e3833fd56e59494694a8d455672d26b29d3a14e0
Sara González <saragonzalezrmrez@gmail.com>
2024-08-04 18:53:00 +0200

    correccion del reproductor

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -9,36 +9,30 @@ export const Pause = () => (
     width="16"
     aria-hidden="true"
     viewBox="0 0 16 16"
-    class="Svg-sc-ytk21e-0 dYnaPI"
+    className="Svg-sc-ytk21e-0 dYnaPI"
   >
     <path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path>
   </svg>
 );
 
-const CurrentSong = ({ image, title, artists}) => {
+const CurrentSong = ({ image, title, artists }) => {
   return (
-    <div
-      className={`
-        flex items-center gap-5 relative
-        overflow-hidden
-        `}>
-          <picture className="w-16 h-16 bg-zinc-800 rounded-md shadow-lg overflow-hidden">
-            <img src={image} alt={title} />
-            </picture>
+    <div className="flex items-center gap-5 relative overflow-hidden">
+      <picture className="w-16 h-16 bg-zinc-800 rounded-md shadow-lg overflow-hidden">
+        <img src={image} alt={title} />
+      </picture>
 
-        <div className="flex flex-col">    
-          <h3 className="font-semibold text-sm block">
-            {title}
-          </h3>
+      <div className="flex flex-col">
+        <h3 className="font-semibold text-sm block">{title}</h3>
 
-          <span className="text-xs opacity-80">
-            {artists?.join(", ")}
-          </span>
-          </div>
-        </div>
-  )
-    
-}
+        <span className="text-xs opacity-80">
+          {artists?.join(", ")}
+        </span>
+      </div>
+    </div>
+  );
+};
+
 export const Play = () => (
   <svg
     role="img"
@@ -46,46 +40,55 @@ export const Play = () => (
     width="16"
     aria-hidden="true"
     viewBox="0 0 16 16"
-    class="Svg-sc-ytk21e-0 dYnaPI"
+    className="Svg-sc-ytk21e-0 dYnaPI"
   >
     <path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"></path>
   </svg>
 );
 
+export const VolumeSilence = () => {
+  <svg data-encore-id="icon" role="presentation" aria-label="Volumen apagado" aria-hidden="true"  viewBox="0 0 16 16" class="Svg-sc-ytk21e-0 kcUFwU"><path d="M13.86 5.47a.75.75 0 0 0-1.061 0l-1.47 1.47-1.47-1.47A.75.75 0 0 0 8.8 6.53L10.269 8l-1.47 1.47a.75.75 0 1 0 1.06 1.06l1.47-1.47 1.47 1.47a.75.75 0 0 0 1.06-1.06L12.39 8l1.47-1.47a.75.75 0 0 0 0-1.06z"></path><path d="M10.116 1.5A.75.75 0 0 0 8.991.85l-6.925 4a3.642 3.642 0 0 0-1.33 4.967 3.639 3.639 0 0 0 1.33 1.332l6.925 4a.75.75 0 0 0 1.125-.649v-1.906a4.73 4.73 0 0 1-1.5-.694v1.3L2.817 9.852a2.141 2.141 0 0 1-.781-2.92c.187-.324.456-.594.78-.782l5.8-3.35v1.3c.45-.313.956-.55 1.5-.694V1.5z"></path></svg>
+}
+export const Volume = () => {
+<svg data-encore-id="icon" role="presentation" aria-label="Volumen bajo" aria-hidden="true"  viewBox="0 0 16 16" class="Svg-sc-ytk21e-0 kcUFwU"><path d="M9.741.85a.75.75 0 0 1 .375.65v13a.75.75 0 0 1-1.125.65l-6.925-4a3.642 3.642 0 0 1-1.33-4.967 3.639 3.639 0 0 1 1.33-1.332l6.925-4a.75.75 0 0 1 .75 0zm-6.924 5.3a2.139 2.139 0 0 0 0 3.7l5.8 3.35V2.8l-5.8 3.35zm8.683 4.29V5.56a2.75 2.75 0 0 1 0 4.88z"></path></svg>
+}
+
 export function Player() {
-  const { currentMusic, isPlaying, setIsPlaying } = usePlayerStore(state => state)
-  const audioRef = useRef();
+  const { currentMusic, isPlaying, setIsPlaying } = usePlayerStore((state) => state);
+  const audioRef = useRef(null);
+  const volumeRef = useRef(1); // Asegúrate de inicializar el ref del volumen
 
-  useEffect(()=>{
-    isPlaying
-    ? audioRef.current.play()
-    : audioRef.current.pause()
-  }, [isPlaying])
+  useEffect(() => {
+    if (isPlaying) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+  }, [isPlaying]);
 
-useEffect(() => {
-  const { song, playlist, songs } = currentMusic
-  if(song) {
-    const src = `/music/${playlist?.id}/0${song.id}.mp3`
-    audioRef.current.src = src
-    audioRef.current.volume = volumeRef.current
-    audioRef.current.play()
-  }
-}, [currentMusic])
+  useEffect(() => {
+    const { song, playlist, songs } = currentMusic;
+    if (song) {
+      const src = `/music/${playlist?.id}/0${song.id}.mp3`;
+      audioRef.current.src = src;
+      audioRef.current.volume = volumeRef.current;
+      audioRef.current.play();
+    }
+  }, [currentMusic]);
+
   const handleClick = () => {
-    setIsPlaying(!isPlaying)
-  } 
+    setIsPlaying(!isPlaying);
+  };
 
   return (
     <div className="flex flex-row justify-between w-full px-4 z-50">
       <div>
-        <CurrentSong {... currentMusic.song} />
-        </div>
+        <CurrentSong {...currentMusic.song} />
+      </div>
 
       <div className="grid place-content-center gap-4 flex-1">
         <div className="flex justify-center">
-          <button
-            className=" bg-white rounded-full p-2"
-            onClick={(handleClick)}>
+          <button className="bg-white rounded-full p-2" onClick={handleClick}>
             {isPlaying ? <Pause /> : <Play />}
             <audio ref={audioRef} />
           </button>
@@ -99,14 +102,13 @@ useEffect(() => {
           min={0}
           className="w-[95px]"
           onValueChange={(value) => {
-            const [newVolume] = value
-            const volumeValue =newVolume / 100
-            volumeRef.current = volumeValue
-            audioRef.current.volume = volumeValue
-          }}/>
+            const [newVolume] = value;
+            const volumeValue = newVolume / 100;
+            volumeRef.current = volumeValue;
+            audioRef.current.volume = volumeValue;
+          }}
+        />
       </div>
-
-
     </div>
   );
 }
